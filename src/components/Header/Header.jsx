08d0ec27e664f95ca1ffb956bdf3444d74adef58
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-20 02:05:29 +0400

    Header component html and styles added, using reactstrap

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './Header.scss';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <header className="main-header">
+                <Navbar className="main-header__nav" color="dark" dark expand="md">
+                    <NavbarBrand className="main-haeder__logo" href="/">GOT React</NavbarBrand>
+
+                    <Nav className="main-header__list ml-auto" navbar>
+                        <NavItem className="main-header__item">
+                            <NavLink className="main-header__link" href="/characters/">Characters</NavLink>
+                        </NavItem>
+                        <NavItem className="main-header__item">
+                            <NavLink className="main-header__link" href="/houses/">Houses</NavLink>
+                        </NavItem>
+                        <NavItem className="main-header__item">
+                            <NavLink className="main-header__link" href="/books/">Books</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            </header>
+        </>
     );
 }
 
