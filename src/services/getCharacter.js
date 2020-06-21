d5fbd22471b702dd8529462ebd7cd62ebabb965b
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-21 16:20:33 +0400

    Random characters list api received

--- src/services/getCharacter.js
+++ src/services/getCharacter.js
@@ -10,7 +10,19 @@ export default class GetCharacter {
         }
     }
 
-    transformCharacter({name, gender, born, died, culture}) {
-        return {name, gender, born, died, culture};
+    async getAllCharacters() {
+        try {
+            const num = Math.floor(Math.random() * 10 + 1);
+            const res = await fetch(`https://www.anapioficeandfire.com/api/characters?page=${num}&pageSize=10`)
+                .then(data => data.json());
+
+            return res.map(this.transformCharacter);
+        } catch (error) {
+
+        }
+    }
+
+    transformCharacter({ name, gender, born, died, culture }) {
+        return { name, gender, born, died, culture };
     }
 }
